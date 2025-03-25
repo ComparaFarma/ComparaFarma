0880f5ecafab0ea95f471dc0fbc3bd9170ad7a35
matheusjus <matheus.plima@trf1.jus.br>
2025-03-25 14:45:04 -0300

    :hammer: chore: Add translations to Supabase

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -10,6 +10,15 @@ export default defineI18nConfig(() => ({
       signup: "Cadastrar",
       profile: "Perfil",
       email: "E-mail",
+      supabaseCodes: {
+        validation_failed: "Credenciais inválidas",
+        user_already_exists: "Usuário já existe",
+        weak_password: "Senha fraca",
+        user_not_found: "Usuário não encontrado",
+        email_exists: "E-mail já cadastrado",
+        email_not_verified: "E-mail não verificado",
+        email_provider_disabled: "Provedor de e-mail desativado",
+      }
     },
   },
 }));
