e54c30fefb6075e32ef1d2f55df18950d6ca8468
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2025-03-29 20:13:55 -0300

    :hammer: config: Add vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,12 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
+    <Analytics />
   </NuxtLayout>
 </template>
 <script setup lang="ts">
 import configureValidate from '@/config/validate';
-
+import { Analytics } from '@vercel/analytics/nuxt'
 configureValidate();
 
 </script>
