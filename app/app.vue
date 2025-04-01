5dd4b09b72168ae7f23c67f51c2b5138debe9717
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2025-04-01 19:09:31 -0300

    :hammer: chore: Add SpeedInsights

--- app/app.vue
+++ app/app.vue
@@ -2,11 +2,13 @@
   <NuxtLayout>
     <NuxtPage />
     <Analytics />
+    <SpeedInsights />
   </NuxtLayout>
 </template>
 <script setup lang="ts">
 import configureValidate from '@/config/validate';
 import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 configureValidate();
 
 </script>
