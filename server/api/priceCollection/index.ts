dc85fe424c83e7795cda012e6073821412ac996d
Matheus Nascimento <matheusnascimentoti99@gmail.com>
2025-04-13 22:12:03 -0300

    :hammer: chore: Add view to get last update

--- server/api/priceCollection/index.ts
+++ server/api/priceCollection/index.ts
@@ -1,7 +1,8 @@
 import { serverSupabaseClient } from '#supabase/server'
 import type { Tables } from '~~/types/database.types'
 export interface PriceCollectionItem extends Tables<'PriceCollection'> {
-    cities: {city: Tables<'City'>}[]
+    cities: { city: Tables<'City'> }[],
+    lastcheckdate: string | undefined
 }
 export default eventHandler(async (event) => {
 
@@ -11,21 +12,36 @@ export default eventHandler(async (event) => {
     const { data, error } = await client
         .from('PriceCollection')
         .select(`*, cities: PriceCollectionCity(
-                city:City(
-                    id,
-                    name,
-                    state
+            city:City(
+                id,
+                name,
+                state
                 )
-            )`
+            )`,
         )
         .order('createdAt', { ascending: false })
         .range(Number(offset), Number(offset) + Number(limit))
+
     if (error) {
         throw createError({ statusCode: 500, statusMessage: error.message })
     }
     if (!data) {
         throw createError({ statusCode: 404, statusMessage: 'No data found' })
     }
-    return data as PriceCollectionItem[]
+
+    const { data: priceCollectionWithDate } = await client
+        .from('view_pricecollection')
+        .select<'*', PriceCollectionItem>('*')
+        .in('id', (data as PriceCollectionItem[]).map(item => item.id))
+
+    console.log('response:' + priceCollectionWithDate)
+
+
+    return (data as PriceCollectionItem[]).map(item => {
+        return {
+            ...item,
+            lastcheckdate: priceCollectionWithDate?.find(e => e.id == item.id)?.lastcheckdate ?? undefined
+        }
+    })
 
 })
